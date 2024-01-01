096fc52c2d7ae87316c7c8395ad1771c932734b7
Unmesh Raj <114986695+TheUnmeshRaj@users.noreply.github.com>
2024-01-01 20:08:26 +0530

    Add files via upload

---
+++ testctf.py
@@ -0,0 +1,11 @@
+flag = ['灩', '捯', '䍔', '䙻', 'ㄶ', '形', "楴", '獟', '楮', '獴', '㌴', '摟', '潦', '弸', '彤', '㔲', '挶', '戹', '㍽']
+
+result = ''
+result2=''
+for i in range(len(flag)):
+    for j in range(126):
+        if 0 < ord(flag[i]) - (j << 8) <= 126:
+            result += chr(j) + chr(ord(flag[i]) - (j << 8))
+            result2 +=(chr(j) + chr(ord(flag[i]) - (j << 8)))
+print(result)
+print(result2)
